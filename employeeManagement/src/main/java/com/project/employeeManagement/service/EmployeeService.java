1f791ecd2c283b66dbfb6036cf431e00ad32b8f7
ardakavas <kavasarda@hotmail.com>
2022-09-02 19:12:27 +0300

    Employee Service added.

---
+++ employeeManagement/src/main/java/com/project/employeeManagement/service/EmployeeService.java
@@ -0,0 +1,9 @@
+package com.project.employeeManagement.service;
+
+import com.project.employeeManagement.model.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+    List<Employee> getAllEmployees();
+}
---
+++ employeeManagement/src/main/java/com/project/employeeManagement/service/EmployeeServiceImpl.java
@@ -0,0 +1,22 @@
+package com.project.employeeManagement.service;
+
+import com.project.employeeManagement.model.Employee;
+import com.project.employeeManagement.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+public class EmployeeServiceImpl implements EmployeeService{
+
+
+
+    private EmployeeRepository employeeRepository;
+    public EmployeeServiceImpl(EmployeeRepository employeeRepository){
+        this.employeeRepository = employeeRepository;
+    }
+
+    @Override
+    public List<Employee> getAllEmployees() {
+        return employeeRepository.findAll();
+    }
+}
