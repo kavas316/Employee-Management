b92dd35ce9f733d55a0b3fde7e803f9692c2ca71
ardakavas <kavasarda@hotmail.com>
2022-09-02 19:16:43 +0300

    Employee Controller added.

---
+++ employeeManagement/src/main/java/com/project/employeeManagement/controller/EmployeeController.java
@@ -0,0 +1,25 @@
+package com.project.employeeManagement.controller;
+
+
+import com.project.employeeManagement.service.EmployeeService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/employees")
+public class EmployeeController {
+
+    private EmployeeService employeeService;
+    public EmployeeController(EmployeeService employeeService){
+        this.employeeService = employeeService;
+    }
+
+    @GetMapping("/home")
+    public String viewHomePage(Model model){
+        model.addAttribute("listEmployess",employeeService.getAllEmployees());
+        return "index";
+    }
+
+}
