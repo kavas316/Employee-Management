2aa0d74818ddfb2dbd15f56d709cc47c00d7a990
ardakavas <kavasarda@hotmail.com>
2022-09-02 18:58:40 +0300

    JPA entity added.

--- employeeManagement/src/main/java/com/project/employeeManagement/model/Employee.java
+++ employeeManagement/src/main/java/com/project/employeeManagement/model/Employee.java
@@ -1,10 +1,20 @@
 package com.project.employeeManagement.model;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "employees")
 public class Employee {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
+    @Column(name="first_name")
     private String firstName;
+    @Column(name="last_name")
     private String lastName;
+    @Column(name="email")
     private String email;
 
     private double salary;
