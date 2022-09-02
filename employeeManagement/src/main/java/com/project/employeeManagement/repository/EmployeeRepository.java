6f7d44b98a2ef047cb05319aa81a887919bcb9dc
ardakavas <kavasarda@hotmail.com>
2022-09-02 19:01:59 +0300

    Repository added.

---
+++ employeeManagement/src/main/java/com/project/employeeManagement/repository/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.project.employeeManagement.repository;
+
+import com.project.employeeManagement.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepository extends JpaRepository<Employee,Long> {
+
+
+}
