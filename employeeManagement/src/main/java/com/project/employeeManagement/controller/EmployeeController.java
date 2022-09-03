19364bb23d5bfb2f6f8b8c02c3e94e0f31fe4cd5
ardakavas <kavasarda@hotmail.com>
2022-09-03 03:32:33 +0300

    Add Employee Method and html file added.

--- employeeManagement/src/main/java/com/project/employeeManagement/controller/EmployeeController.java
+++ employeeManagement/src/main/java/com/project/employeeManagement/controller/EmployeeController.java
@@ -1,11 +1,14 @@
 package com.project.employeeManagement.controller;
 
 
+import com.project.employeeManagement.model.Employee;
 import com.project.employeeManagement.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -24,4 +27,18 @@ public class EmployeeController {
         return "index";
     }
 
+    @GetMapping("/new_employee")
+    public String newEmployeeForm(Model model){
+        // Create Model for form data
+        Employee employee = new Employee();
+        model.addAttribute("employee",employee);
+        return "new_employee";
+    }
+
+    @PostMapping("/saveEmployee")
+    public String saveEmployee(@ModelAttribute("employee")Employee employee){
+        // Save Employee from form to database
+        employeeService.saveEmployee(employee);
+        return "redirect:/";
+    }
 }
